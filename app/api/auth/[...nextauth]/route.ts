a90149e3c2dc64d2c3872c98bdc8c3adc27d08fd
Will Olson <will.olson11@gmail.com>
2024-03-23 23:31:26 +0000

    Replace magic link with OTP in the signin verification email

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,11 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth, { AuthOptions, Theme } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import { Adapter } from "next-auth/adapters";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import prisma from "@/app/_lib/prisma";
 import { randomInt } from "crypto";
+import { createTransport } from "nodemailer";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
@@ -23,6 +24,22 @@ export const authOptions: AuthOptions = {
       async generateVerificationToken() {
         return gernerateOTP().toString()
       },
+      async sendVerificationRequest({
+        identifier: email,
+        token,
+        url,
+        provider: { server, from },
+      }) {
+        const { host } = new URL(url)
+        const transport = createTransport(server)
+        await transport.sendMail({
+          to: email,
+          from,
+          subject: `Sign in to ${host}`,
+          text: text({ token, host }),
+          html: html({ token, host }),
+        })
+      }
     }),
   ],
   pages: {
@@ -33,6 +50,57 @@ export const authOptions: AuthOptions = {
 function gernerateOTP() {
   return randomInt(100000, 999999);
 };
+
+function html(params: { token: string; host: string; }) {
+  const { token, host } = params
+
+  const escapedHost = host.replace(/\./g, "&#8203;.")
+
+  const color = {
+    background: "#f9f9f9",
+    text: "#444",
+    mainBackground: "#fff",
+  }
+
+  return `
+<body style="background: ${color.background};">
+  <table width="100%" border="0" cellspacing="20" cellpadding="0"
+    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
+    <tr>
+      <td align="center"
+        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        Sign in to <strong>${escapedHost}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 20px 0;">
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center"><strong>Sign in code:</strong> ${token}</td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center"
+        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        Keep in mind that this code will expire after <strong><em>3 minutes</em></strong>. If you did not request this email you can safely ignore it.
+      </td>
+    </tr>
+  </table>
+</body>
+  `;
+}
+
+function text(params: { token: string, host: string }) {
+  return `
+  Sign in to ${params.host}
+  
+  Sign in code: ${params.token}
+  
+  Keep in mind that this code will expire after 3 minutes. If you did not request this email you can safely ignore it.
+  `;
+}
 
 const handler =  NextAuth(authOptions);
 export { handler as GET, handler as POST }
